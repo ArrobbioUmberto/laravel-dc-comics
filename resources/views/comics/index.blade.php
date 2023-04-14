f1cac6262e374cc2a2fb9957ef3c76c72564269b
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-14 16:08:04 +0200

    FEAT layouts and partials

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section
+<div class="container">
+    <div class="row">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">First</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Mark</td>
+                </tr>
+                <tr>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
