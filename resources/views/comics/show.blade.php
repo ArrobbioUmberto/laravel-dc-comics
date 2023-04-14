3f4b4bcbf6c98dcfa47ce25dea8f70452ede2658
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-14 17:30:35 +0200

    FEAT show blade add

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="card"">
+        <img src=" {{$comic->thumb}}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{$comic->title}}</h5>
+            <p class="card-text"><strong>Descrizione:</strong> {{$comic->description}}</p>
+            <p class="card-text"><strong>Prezzo:</strong> {{$comic->price}}</p>
+            <p class="card-text"><strong>Serie:</strong> {{$comic->series}}</p>
+            <p class="card-text"><strong>Data d'uscita:</strong> {{$comic->sale_date}}</p>
+            <p class="card-text"><strong>Genere:</strong> {{$comic->type}}</p>
+        </div>
+    </div>
+</div>
+@endsection
