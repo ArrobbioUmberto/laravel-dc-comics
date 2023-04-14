de262f49ee1c9732d22c103851a5bf05b00a5504
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-14 17:30:23 +0200

    Change POSITION FOR A LINK

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,22 +4,26 @@
     <div class="row">
         <table class="table">
             @foreach ($comics as $comic)
+
             <thead>
                 <tr>
                     <th scope="col">{{$comic->id}}</th>
                 </tr>
             </thead>
+
             <tbody>
                 <tr>
-                    <td><strong>Titolo:</strong> {{$comic->title}}</td>
+                    <td><strong>Titolo:</strong><a href="{{route('comics.show', $comic->id)}}"> {{$comic->title}}</a></td>
                     <td><strong>Descrizione:</strong> {{$comic->description}}</td>
                     <td><img src="{{$comic->thumb}}" alt=""></td>
                     <td><strong>Prezzo:</strong> {{$comic->price}}</td>
                     <td><strong>Serie:</strong> {{$comic->series}}</td>
                     <td><strong>Data d'uscita:</strong> {{$comic->sale_date}}</td>
                     <td><strong>Genere:</strong> {{$comic->type}}</td>
+
                 </tr>
             </tbody>
+
             @endforeach
         </table>
     </div>
