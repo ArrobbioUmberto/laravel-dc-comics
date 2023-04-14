e2d4d0c890e062576ab93fb2dd1f402cf00f8a9d
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-14 16:53:31 +0200

    FEAT index route

--- database/seeders/ComicsSeeder.php
+++ database/seeders/ComicsSeeder.php
@@ -128,12 +128,12 @@ class ComicsSeeder extends Seeder
         foreach ($comics as $comic) {
             $new_comic = new Comic();
             $new_comic->title = $comic['title'];
-            $new_comic->title = $comic['description'];
-            $new_comic->title = $comic['thumb'];
-            $new_comic->title = $comic['price'];
-            $new_comic->title = $comic['series'];
-            $new_comic->title = $comic['sales_date'];
-            $new_comic->title = $comic['type'];
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
             $new_comic->save();
         }
     }
