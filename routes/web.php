7a15bfa7645d7e0b925e592e25397ff6d0b8d7f7
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-14 17:31:05 +0200

    FEAT add route for show

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
-Route::get('comics/{comic->title}', [ComicController::class, 'show'])->name('comics.show');
+Route::get('comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
