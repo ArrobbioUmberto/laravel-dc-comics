9fe09044125ee6fb5781eeb4504c295797f9f985
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-18 16:28:29 +0200

    FEAT add button back homepage

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     <div class="card"">
-        <img src=" {{$comic->thumb}}" class="card-img-top" alt="...">
+        <img src=" {{$comic->thumb}}" class="card-img-top " alt="...">
         <div class="card-body">
             <h5 class="card-title">{{$comic->title}}</h5>
             <p class="card-text"><strong>Descrizione:</strong> {{$comic->description}}</p>
@@ -12,6 +12,7 @@
             <p class="card-text"><strong>Serie:</strong> {{$comic->series}}</p>
             <p class="card-text"><strong>Data d'uscita:</strong> {{$comic->sale_date}}</p>
             <p class="card-text"><strong>Genere:</strong> {{$comic->type}}</p>
+            <a href="{{route('comics.index')}}" class="btn btn-secondary"> Torna alla Homepage</a>
         </div>
     </div>
 </div>
