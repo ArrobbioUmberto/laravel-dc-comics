88b6cb515e68da91e0984646e99291b3404120bb
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-18 14:49:52 +0200

    FEAT function destroy

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -63,4 +63,9 @@ class ComicController extends Controller
 
         return to_route('comics.show', $comic);
     }
+    public function destroy(Comic $comic)
+    {
+        $comic->delete();
+        return to_route('comics.index');
+    }
 }
