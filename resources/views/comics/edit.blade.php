c681f5a956b64bca1c59f5eddc38a2dea9ea1084
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-19 19:02:46 +0200

    FEAT add is invalid for error

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -10,7 +10,7 @@
         <div class="input-group mb-3">
             <label for="title" class="form-label">Titolo</label>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="title" name="title" value="{{old('title',$comic->title)}}">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="title" name="title" value="{{old('title',$comic->title)}}">
                 @error('title')
                 <div class="invalid-feedback">
                     {{$message}}
@@ -22,10 +22,10 @@
         <div class="input-group mb-3">
             <label for="description" class="form-label">Descrizione</label>
             <div class="input-group">
-                <textarea class="form-control" id="description" name="description" cols="30" rows="10">
-                {{$comic->description}}
+                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" cols="30" rows="10" value="{{old('descrption',$comic->description)}}">
+                {{old('description',$comic->description)}}
                 </textarea>
-                @error('title')
+                @error('description')
                 <div class="invalid-feedback">
                     {{$message}}
                 </div>
@@ -35,7 +35,7 @@
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Immagine di copertina (url)</label>
-            <input class="form-control" type="text" id="thumb" name="thumb" value="{{old('thumb',$comic->thumb)}}">
+            <input class="form-control @error('thumb') is-invalid @enderror" type="text" id="thumb" name="thumb" value="{{old('thumb',$comic->thumb)}}">
             @error('thumb')
             <div class="invalid-feedback">
                 {{$message}}
@@ -47,7 +47,7 @@
             <label for="price" class="form-label">Prezzo</label>
             <div class="input-group">
                 <span class="input-group-text">$</span>
-                <input type="text" placeholder="" class="form-control" aria-label="Amount (to the nearest dollar)" id="price" name="price" value="{{old('price',$comic->price)}}">
+                <input type="text" placeholder="" class="form-control @error('price') is-invalid @enderror" aria-label="Amount (to the nearest dollar)" id="price" name="price" value="{{old('price',$comic->price)}}">
                 @error('price')
                 <div class="invalid-feedback">
                     {{$message}}
@@ -59,7 +59,7 @@
         <div class="input-group mb-3">
             <label for="series" class="form-label">Serie</label>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="" aria-label="Username" id="series" name="series" value="{{old('series',$comic->series)}}">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" placeholder="" aria-label="Username" id="series" name="series" value="{{old('series',$comic->series)}}">
                 @error('series')
                 <div class="invalid-feedback">
                     {{$message}}
@@ -71,7 +71,7 @@
         <div class="input-group mb-3">
             <label for="sale-date" class="form-label">Data d'uscita</label>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="" aria-label="Username" id="sale-date" name="sale_date" value="{{old('sale_date',$comic->sale_date)}}">
+                <input type="text" class="form-control @error('sale_date') is-invalid @enderror" placeholder="" aria-label="Username" id="sale-date" name="sale_date" value="{{old('sale_date',$comic->sale_date)}}">
                 @error('sale_date')
                 <div class="invalid-feedback">
                     {{$message}}
@@ -83,7 +83,7 @@
         <div class="input-group">
             <label for="type" class="form-label">Genere</label>
             <div class="input-group">
-                <select class="form-select" id="type" name="type" value="{{old('type',$comic->type)}}" aria-label="Default select example">
+                <select class="form-select @error('type') is-invalid @enderror" id="type" name="type" value="{{old('type',$comic->type)}}" aria-label="Default select example">
                     <option>Scegli il genere</option>
                     <option @selected ( old('type')=='comic book' ) value="comic book">Comic Book</option>
                     <option @selected ( old('type')=='graphic novel' ) value="graphic novel">Graphic Novel</option>
@@ -96,8 +96,9 @@
             </div>
         </div>
 
-        <div class="col-auto mt-2">
-            <button type="submit" class="btn btn-primary mb-3">Modifica</button>
+        <div class="col-auto mt-3">
+            <button type="submit" class="btn btn-primary">Modifica</button>
+            <a href="{{route('comics.index')}}" class="btn btn-secondary"> Torna alla Homepage</a>
         </div>
     </form>
 </div>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
 <div class="container">
-    <div class="card"">
-        <img src=" {{$comic->thumb}}" class="card-img-top " alt="...">
-        <div class="card-body">
+    <div class="card flex-row ">
+        <img src=" {{$comic->thumb}}" class="card-img-top" style="width:500px;" alt="...">
+        <div class="card-body ms-4">
             <h5 class="card-title">{{$comic->title}}</h5>
             <p class="card-text"><strong>Descrizione:</strong> {{$comic->description}}</p>
             <p class="card-text"><strong>Prezzo:</strong> {{$comic->price}}</p>
