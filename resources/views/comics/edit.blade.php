9d53c0427a78c3ad7493af056d502439612e1166
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-19 16:31:29 +0200

    FEAT add error message

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -11,6 +11,11 @@
             <label for="title" class="form-label">Titolo</label>
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="title" name="title" value="{{old('title',$comic->title)}}">
+                @error('title')
+                <div class="invalid-feedback">
+                    {{$$message}}
+                </div>
+                @enderror
             </div>
         </div>
 
@@ -20,12 +25,22 @@
                 <textarea class="form-control" id="description" name="description" cols="30" rows="10">
                 {{$comic->description}}
                 </textarea>
+                @error('title')
+                <div class="invalid-feedback">
+                    {{$$message}}
+                </div>
+                @enderror
             </div>
         </div>
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Immagine di copertina (url)</label>
             <input class="form-control" type="text" id="thumb" name="thumb" value="{{old('thumb',$comic->thumb)}}">
+            @error('thumb')
+            <div class="invalid-feedback">
+                {{$$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="input-group mb-3">
@@ -33,6 +48,11 @@
             <div class="input-group">
                 <span class="input-group-text">$</span>
                 <input type="text" placeholder="" class="form-control" aria-label="Amount (to the nearest dollar)" id="price" name="price" value="{{old('price',$comic->price)}}">
+                @error('price')
+                <div class="invalid-feedback">
+                    {{$$message}}
+                </div>
+                @enderror
             </div>
         </div>
 
@@ -40,6 +60,11 @@
             <label for="series" class="form-label">Serie</label>
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="" aria-label="Username" id="series" name="series" value="{{old('series',$comic->series)}}">
+                @error('series')
+                <div class="invalid-feedback">
+                    {{$$message}}
+                </div>
+                @enderror
             </div>
         </div>
 
@@ -47,6 +72,11 @@
             <label for="sale-date" class="form-label">Data d'uscita</label>
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="" aria-label="Username" id="sale-date" name="sale_date" value="{{old('sale_date',$comic->sale_date)}}">
+                @error('sale_date')
+                <div class="invalid-feedback">
+                    {{$$message}}
+                </div>
+                @enderror
             </div>
         </div>
 
@@ -54,6 +84,11 @@
             <label for="type" class="form-label">Genere</label>
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="type" name="type" value="{{old('type',$comic->type)}}">
+                @error('type')
+                <div class="invalid-feedback">
+                    {{$$message}}
+                </div>
+                @enderror
             </div>
         </div>
 
