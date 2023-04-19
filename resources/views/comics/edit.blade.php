abf44af5966bc9610edb7f711c2c9c8d8a864621
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-19 16:27:46 +0200

    FEAT add old in edit blade

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -10,7 +10,7 @@
         <div class="input-group mb-3">
             <label for="title" class="form-label">Titolo</label>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="title" name="title" value="{{$comic->title}}">
+                <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="title" name="title" value="{{old('title',$comic->title)}}">
             </div>
         </div>
 
@@ -25,35 +25,35 @@
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Immagine di copertina (url)</label>
-            <input class="form-control" type="text" id="thumb" name="thumb" value="{{$comic->thumb}}">
+            <input class="form-control" type="text" id="thumb" name="thumb" value="{{old('thumb',$comic->thumb)}}">
         </div>
 
         <div class="input-group mb-3">
             <label for="price" class="form-label">Prezzo</label>
             <div class="input-group">
                 <span class="input-group-text">$</span>
-                <input type="text" placeholder="" class="form-control" aria-label="Amount (to the nearest dollar)" id="price" name="price" value="{{$comic->price}}">
+                <input type="text" placeholder="" class="form-control" aria-label="Amount (to the nearest dollar)" id="price" name="price" value="{{old('price',$comic->price)}}">
             </div>
         </div>
 
         <div class="input-group mb-3">
             <label for="series" class="form-label">Serie</label>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="" aria-label="Username" id="series" name="series" value="{{$comic->series}}">
+                <input type="text" class="form-control" placeholder="" aria-label="Username" id="series" name="series" value="{{old('series',$comic->series)}}">
             </div>
         </div>
 
         <div class="input-group mb-3">
             <label for="sale-date" class="form-label">Data d'uscita</label>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="" aria-label="Username" id="sale-date" name="sale_date" value="{{$comic->sale_date}}">
+                <input type="text" class="form-control" placeholder="" aria-label="Username" id="sale-date" name="sale_date" value="{{old('sale_date',$comic->sale_date)}}">
             </div>
         </div>
 
         <div class="input-group">
             <label for="type" class="form-label">Genere</label>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="type" name="type" value="{{$comic->type}}">
+                <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="type" name="type" value="{{old('type',$comic->type)}}">
             </div>
         </div>
 
