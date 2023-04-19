eaf6c43cb0c5d024e33ecaf94c6536b504aacb39
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-19 16:08:59 +0200

    FEAT add validation on store and update

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use App\Models\Comic;
 use Illuminate\Http\Request;
+use illuminate\Validation\Rule;
 
 class ComicController extends Controller
 {
@@ -21,21 +22,33 @@ class ComicController extends Controller
     }
     public function store(Request $request)
     {
-        $data = $request->all();
+        $data = $request->validate([
+            'title' => 'required|unique|max:255|min:3',
+            'description' => 'nullable|string|max:255|min:3',
+            'thumb' => 'required|max:255|url',
+            'price' => 'required|max:255|min:3',
+            'series' => 'required|max:255|min:3',
+            'sale_date' => 'required|date',
+            'type' => [
+                'required',
+                Rule::in(['comic book', 'graphic novel'])
+            ],
+        ]);
 
-        $last_comic = new Comic();
-        $last_comic->title = $data['title'];
-        $last_comic->description = $data['description'];
-        $last_comic->thumb = $data['thumb'];
-        $last_comic->price = $data['price'];
-        $last_comic->series = $data['series'];
-        $last_comic->sale_date = $data['sale_date'];
-        $last_comic->type = $data['type'];
 
-        $last_comic->save();
+        // $last_comic = new Comic();
+        // $last_comic->title = $data['title'];
+        // $last_comic->description = $data['description'];
+        // $last_comic->thumb = $data['thumb'];
+        // $last_comic->price = $data['price'];
+        // $last_comic->series = $data['series'];
+        // $last_comic->sale_date = $data['sale_date'];
+        // $last_comic->type = $data['type'];
+
+        $last_comic = Comic::create($data);
 
 
-        return redirect()->route('comics.show', $last_comic->id);
+        return redirect()->route('comics.show', $last_comic);
     }
     public function show(Comic $comic)
     {
@@ -47,17 +60,28 @@ class ComicController extends Controller
     }
     public function update(Request $request, Comic $comic)
     {
-        $data = $request->all();
+        $data = $request->validate([
+            'title' => 'required|unique|max:255|min:3',
+            'description' => 'nullable|string|max:255|min:3',
+            'thumb' => 'required|max:255|url',
+            'price' => 'required|max:255|min:3',
+            'series' => 'required|max:255|min:3',
+            'sale_date' => 'required|date',
+            'type' => [
+                'required',
+                Rule::in(['comic book', 'graphic novel'])
+            ],
+        ]);
 
 
-        $comic->title = $data['title'];
-        $comic->description = $data['description'];
-        $comic->thumb = $data['thumb'];
-        $comic->price = $data['price'];
-        $comic->series = $data['series'];
-        $comic->sale_date = $data['sale_date'];
-        $comic->type = $data['type'];
-
+        // $comic->title = $data['title'];
+        // $comic->description = $data['description'];
+        // $comic->thumb = $data['thumb'];
+        // $comic->price = $data['price'];
+        // $comic->series = $data['series'];
+        // $comic->sale_date = $data['sale_date'];
+        // $comic->type = $data['type'];
+        $comic->fill($data);
         $comic->save();
 
 
